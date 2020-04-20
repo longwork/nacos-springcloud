7e33c24e7db4b16342fff8d8cb2e6c678ce0b590
longwork <1749560162@qq.com>
2020-04-20 17:57:41 +0800

    Monday-afternoon commit

--- src/main/java/wimi/nacosspring/CombinedLanguageLegal1225.java
+++ src/main/java/wimi/nacosspring/CombinedLanguageLegal1225.java
@@ -1,5 +1,7 @@
 package wimi.nacosspring;
 
+import java.io.PrintWriter;
+
 import java.util.Scanner;
 
 /**
@@ -7,27 +9,31 @@ import java.util.Scanner;
  * @date 2020-04-19 20:25:24 周日
  */
 public class CombinedLanguageLegal1225 {
+    static Scanner in = new Scanner(System.in);
+    static PrintWriter out = new PrintWriter(System.out);
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         in.nextLine();
         while (n-- > 0) {
             String str = in.nextLine();
-            char a = str.charAt(0);
-            int i = a - '0';
-            boolean flag = (a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z');
-            if (i >= 0 && i <= 9) {
-                System.out.println("no");
-            } else if (flag || a == '_') {
-                String pattern = "[0-9a-zA-z_]";
-                String[] split = str.split(pattern);
-                if (split.length == 0) {
-                    System.out.println("yes");
-                } else {
-                    System.out.println("no");
-                }
+            if (match(str)) {
+                out.println("yes");
+            } else {
+                out.println("no");
             }
         }
+        out.close();
         in.close();
+    }
+
+    private static boolean match(String str) {
+        String s = str.substring(0, 1);
+        String regex = "[a-zA-z_]";
+        if (!s.matches(regex)) {
+            return false;
+        }
+        String[] split = str.split("[0-9a-zA-Z_]");
+        return split.length == 0;
     }
 }
