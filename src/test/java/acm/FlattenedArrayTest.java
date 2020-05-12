74de2d5010c9e8931d84481a2c2ee67d8708e5d6
username <email>
2020-05-12 22:50:12 +0800

    2020-05-12-Tuesday commit

--- src/test/java/acm/FlattenedArrayTest.java
+++ src/test/java/acm/FlattenedArrayTest.java
@@ -2,9 +2,7 @@ package acm;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 
 /**
@@ -12,59 +10,59 @@ import java.util.stream.Collectors;
  * @date : Created in 2020/05/12 16:10
  */
 public class FlattenedArrayTest {
-    private static final Scanner CIN = new Scanner(System.in);
+    //private static final Scanner CIN = new Scanner(System.in);
 
     @Test
     public void test1() {
-        while (CIN.hasNext()) {
-            List<String> numList = new ArrayList<>();
-            int n = CIN.nextInt();
-            int temp;
-            for (int i = 0; i < n; i++) {
-                temp = CIN.nextInt();
-                numList.add(String.valueOf(temp));
-            }
-
-            Runtime r = Runtime.getRuntime();
-            r.gc();
-            long startMemory = r.freeMemory();
-            long startTime = System.currentTimeMillis();
-
-            sortList(numList);
-
-            long endTime = System.currentTimeMillis();
-            long endMemory = r.freeMemory();
-
-            System.out.println((endTime - startTime) / 1000.0);
-            System.out.println((startMemory - endMemory) / 1000.0);
-        }
-        CIN.close();
+//        while (CIN.hasNext()) {
+//            List<String> numList = new ArrayList<>();
+//            int n = CIN.nextInt();
+//            int temp;
+//            for (int i = 0; i < n; i++) {
+//                temp = CIN.nextInt();
+//                numList.add(String.valueOf(temp));
+//            }
+//
+//            Runtime r = Runtime.getRuntime();
+//            r.gc();
+//            long startMemory = r.freeMemory();
+//            long startTime = System.currentTimeMillis();
+//
+//            sortList(numList);
+//
+//            long endTime = System.currentTimeMillis();
+//            long endMemory = r.freeMemory();
+//
+//            System.out.println((endTime - startTime) / 1000.0);
+//            System.out.println((startMemory - endMemory) / 1000.0);
+//        }
+//        CIN.close();
     }
 
     @Test
     public void test2() {
-        while (CIN.hasNext()) {
-            int n = CIN.nextInt();
-            String[] strings = new String[n];
-            int temp;
-            for (int i = 0; i < n; i++) {
-                temp = CIN.nextInt();
-                strings[i] = String.valueOf(temp);
-            }
-            Runtime r = Runtime.getRuntime();
-            r.gc();
-
-            long startMemory = r.freeMemory();
-            long startTime = System.currentTimeMillis();
-
-            sortArray(strings);
-            long endTime = System.currentTimeMillis();
-            long endMemory = r.freeMemory();
-
-            System.out.println((endTime - startTime) / 1000.0);
-            System.out.println((startMemory - endMemory) / 1000.0);
-        }
-        CIN.close();
+//        while (CIN.hasNext()) {
+//            int n = CIN.nextInt();
+//            String[] strings = new String[n];
+//            int temp;
+//            for (int i = 0; i < n; i++) {
+//                temp = CIN.nextInt();
+//                strings[i] = String.valueOf(temp);
+//            }
+//            Runtime r = Runtime.getRuntime();
+//            r.gc();
+//
+//            long startMemory = r.freeMemory();
+//            long startTime = System.currentTimeMillis();
+//
+//            sortArray(strings);
+//            long endTime = System.currentTimeMillis();
+//            long endMemory = r.freeMemory();
+//
+//            System.out.println((endTime - startTime) / 1000.0);
+//            System.out.println((startMemory - endMemory) / 1000.0);
+//        }
+//        CIN.close();
     }
 
     private static void sortList(List<String> numList) {
